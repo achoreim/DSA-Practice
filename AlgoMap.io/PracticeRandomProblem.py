b842f54066e91c0e0a4ac53d4bfb9123d1fc6d78
Ahmad Choreim <AhmadChoreim@gmail.com>
2025-04-03 16:27:12 -0400

    Adding initial commit for PracticeRandomProblems.py

---
+++ AlgoMap.io/PracticeRandomProblem.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+"""
+This Python Script contains a database of links for all completed LeetCode Questions so far on 
+the AlgoMap.io List.
+
+The purpose of this script is to be run everyday in order to practice random problems that I have
+already completed for the sake of revision.
+"""
+
+url = "https://leetcode.com/problems/roman-to-integer/"
+webbrowser.open(url)
