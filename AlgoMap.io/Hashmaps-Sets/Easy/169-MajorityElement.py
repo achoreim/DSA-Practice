185d1d4ea8ca88799a151baa1bd99b0b4b4b416d
Ahmad Choreim <AhmadChoreim@gmail.com>
2025-04-22 14:54:50 -0400

    Adding second solution, and notes

--- AlgoMap.io/Hashmaps-Sets/Easy/169-MajorityElement.py
+++ AlgoMap.io/Hashmaps-Sets/Easy/169-MajorityElement.py
@@ -3,7 +3,7 @@ Easy
 169. Majority Element
 https://leetcode.com/problems/majority-element/description/
 
-Time for first solution:
+Time for first solution O(n): 3:08 
 Time to compile all solutions and notes:
 """
 
@@ -35,7 +35,7 @@ n == nums.length
 """
 
 """
-My first solution: O(n)
+My first solution: Uses the counter method and loops throgh it keeping track of the highest count
 Time Complexity: O(n)
 Space Complecity: O(n)
 """
@@ -53,3 +53,43 @@ class Solution:
                 majority_count = num_count[x]
         
         return majority_element
+    
+"""
+My second solution: (After understanding the Boyer-Moore Voting Algorithm):
+Time COmplexity; O(n)
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for x in nums: 
+            if count == 0:
+                candidate = x
+            if x == candidate:
+                count +=1
+            else:
+                count -= 1
+
+"""
+Notes:
+
+    * What I have learned:
+        - Boyer-Moore Voting Algorithm: It is a neat algorithm when the contraints suggest that there is an
+          element which apears 
+
+    * What I can Improve on:
+        - Figure out a way to loop without initializing the first elements to out of scope variables
+          This strategy seems to be working for now, however I do not belive that it is the industry standard.
+
+    * Miscelanious:
+        - While attempting to solve this problem, I thought of a unique method that might  be great
+          to write down: 
+          If there was a way to sort the array in O(n) using counting sort, then we can probe 4 times at 25% intervals
+          of the sorted array, since the majority element appears more than 50% of the time, it is guarranteed that
+          two of the 4 probes would be the same number, and therefore the majority element.
+          
+
+"""
