f70f5d56a2871fd4678bfbae194ccb14ebc6646d
Ahmad Choreim <AhmadChoreim@gmail.com>
2025-04-26 20:52:26 -0400

    Adding Second solution to 125-ValidPalindrome.py

--- AlgoMap.io/two-pointers/Easy/125-ValidPalindrome.py
+++ AlgoMap.io/two-pointers/Easy/125-ValidPalindrome.py
@@ -85,7 +85,44 @@ class Solution:
 
         return True
             
+"""
+My second solution:
+The same as my first solution, but implementing the 'continue' keyword inside the while loop:
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        1. convert upper case to lower case
+        2. Remove all non-alphanumeric characters
+        3. .isalnum()
+        """
+
+        # Return true if s = ""
+        if not s:
+            return True
+
+        left = 0
+        right = len(s) - 1
+
+        while left <= right:
             
+            if s[left].isalnum() and s[right].isalnum():
+                
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+                continue
+            
+            if not s[left].isalnum():
+                left += 1
+            if not s[right].isalnum():
+                right -= 1
+
+            
+
+        return True
 
 
 """
@@ -94,6 +131,8 @@ Notes:
     * What I Learned:
         - .isalnum() => A method that returns true or false if the given
                         character or string is alphanumeric or not.
+        - If you use the 'continue' keyword inside a while loop, it will skip the rest of
+          the while loop and restart another iteration.
 
     * What I can work on:
         - Iterating through the while loop: You need to make sure that you 
