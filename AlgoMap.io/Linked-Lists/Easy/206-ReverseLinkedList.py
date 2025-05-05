1605852cc0288a4778157ff4c0057d0fea9a827d
Ahmad Choreim <AhmadChoreim@gmail.com>
2025-05-05 15:54:29 -0400

    Added Noted to 206-ReverseLinkedList.py

--- AlgoMap.io/Linked-Lists/Easy/206-ReverseLinkedList.py
+++ AlgoMap.io/Linked-Lists/Easy/206-ReverseLinkedList.py
@@ -3,7 +3,7 @@ Easy
 206. Reverse Linked List
 https://leetcode.com/problems/reverse-linked-list/description/
 
-Time for first solution:
+Time for first solution: 6:06
 Time to compile all solutions and notes: 
 """
 
@@ -28,3 +28,38 @@ Input: head = []
 Output: []
 
 """
+
+"""
+My Initial Solution:
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        
+        return prev
+
+"""
+Notes:
+
+    * What I learned:
+        - Startegy: Keeping a temp variable and iterate through the linkedlist with a while loop.
+
+    * What I can work on:
+        - Learn about dummy nodes.
+"""
+
+
